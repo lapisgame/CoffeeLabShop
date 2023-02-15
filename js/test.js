3e22bc13738ce6ee14a2e79633b05b0ec31c8636
lapis <klimantovartem@gmail.com>
2023-02-16 00:50:06 +0300

    Попытка сделать добавление и вывод продукта в бд и обратно (вроде работает)

--- js/test.js
+++ js/test.js
@@ -1,9 +1,6 @@
 let btn = document.querySelector(".btn");
-let uid
 
-auth.onAuthStateChanged((user) => {
-    uid = user.uid;
-})
+const form = document.querySelector("#createProduct")
 
 btn.addEventListener('click', e => {
     e.preventDefault();
@@ -20,4 +17,55 @@ btn.addEventListener('click', e => {
             title: 'Не ок'
         })
     })
+})
+
+function saveData(){
+    const form = document.querySelector("#createProduct")
+
+    const name = form['name'].value;
+    const dis = form['discription'].value;
+    const par1 = form['par1'].value;
+    const par2 = form['par2'].value;
+    const par3 = form['par3'].value;
+
+    return db.collection('Prod').doc(name).set({
+        name,
+        dis,
+        par1,
+        par2,
+        par3
+    }).then(() => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Ок',
+        })
+    }).then(() => {
+        drawProducts();
+    }).catch(() => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Не ок'
+        })
+    })
+}
+
+function drawProducts(name){
+    db.collection('Prod').doc(name).get().then(doc => {
+        document.querySelector('#nameRow').innerHTML = (doc.data().name);
+        document.querySelector('#dis').innerHTML = (doc.data().dis);
+
+        document.querySelector('#r1').innerHTML = (doc.data().par1);
+        document.querySelector('#r2').innerHTML = (doc.data().par2);
+        document.querySelector('#r3').innerHTML = (doc.data().par3);
+    })
+}
+
+document.querySelector("#createProduct").addEventListener('submit', e=>{
+    e.preventDefault();
+    saveData();
+})
+
+document.querySelector("#draw").addEventListener('submit', e=>{
+    e.preventDefault();
+    drawProducts(document.querySelector("#nameDraw").value);
 })
