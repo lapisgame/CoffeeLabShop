fe9f3d9b88b18eca45855e44af2f6655b5f45158
lapis <klimantovartem@gmail.com>
2023-02-28 23:36:19 +0300

    Кнопка еще работает, кнопки корректны(прописаны руками), каталог переносит на страницу с параметрами

--- js/load_product_page.js
+++ js/load_product_page.js
@@ -1,61 +1,106 @@
 function load(name){
-    let arrMix = new Array(6);
-    let arrMono = new Array(6);
-    let arrSpec = new Array(6);
+    console.log(name);
 
-    let count1=0;
-    let count2=0;
-    let count3=0;
+    
+    db.collection('Prod').doc(name).get().then((el) => {
+        if (el.data().Type === "mix"){
+            let a = document.querySelector('#all1')
+            a.setAttribute('href','Product_page.html?name=Arpeggio')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Arpeggio";
 
-    db.collection('Prod').get().then((el) => {
-        for (let i=0; i<el.docs.length; i++){
-            if (el.docs[i] !== undefined){
-                let id = el.docs[i].id;
-                db.collection('Prod').doc(id).get().then((elem) => {
-                    if (elem.data().Type === "mix"){
-                        arrMix[count1] = elem.data().Name;
-                        console.log(count1, arrMix[count1]);
-                        count1++;
-                    }
-                    else if (elem.data().Type === "mono"){
-                        arrMono[count2] = elem.data().Name;
-                        count2++;
-                    }
-                    else{
-                        arrSpec[count3] = elem.data().Name;
-                        count3++;
-                    }   
-                })
-            }
+            a = document.querySelector('#all2')
+            a.setAttribute('href','Product_page.html?name=Capriccio')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Capriccio";
+
+            a = document.querySelector('#all3')
+            a.setAttribute('href','Product_page.html?name=Esperanza')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Esperanza";
+
+            a = document.querySelector('#all4')
+            a.setAttribute('href','Product_page.html?name=Fugace')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Fugace";
+
+            a = document.querySelector('#all5')
+            a.setAttribute('href','Product_page.html?name=Милано')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Милано";
+
+            a = document.querySelector('#all6')
+            a.setAttribute('href','Product_page.html?name=Портофино')
+            a.querySelector("#img").setAttribute('src','img/first_img.png')
+            a.querySelector('#p').innerHTML = "Портофино";
+
         }
-    }).then(() => {
+        else if (el.data().Type === "mono"){
+            let a = document.querySelector('#all1')
+            a.setAttribute('href','Product_page.html?name=Бразилия')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Бразилия";
 
-        db.collection('Prod').doc(name).get().then((el) => {
-            if (el.data().Type === "mix"){
-                for (let i=0; i<6; i++){
-                    const a = document.querySelector(`#all${i+1}`)
-                    console.log(arrMix[i]);
-                    a.setAttribute('href',`Product_page.html?name=${arrMix[i]}`)
-                    a.querySelector("#img").setAttribute('src','img/first_img.png')
-                }
-            }
-            else if (el.data().Type === "mono"){
-                for (let i=0; i<6; i++){
-                    const a = document.querySelector(`#all${i+1}`)
-                    a.setAttribute('href',`Product_page.html?name=${arrMono[i]}`)
-                    a.querySelector("#img").setAttribute('src','img/first_img.png')
-                }
-            }
-            else{
-                for (let i=0; i<6; i++){
-                    const a = document.querySelector(`#all${i+1}`)
-                    a.setAttribute('href',`Product_page.html?name=${arrSpec[i]}`)
-                    a.querySelector("#img").setAttribute('src','img/first_img.png')
-                }
-            }
-        })
+            a = document.querySelector('#all2')
+            a.setAttribute('href','Product_page.html?name=Гватемала')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Гватемала";
+
+            a = document.querySelector('#all3')
+            a.setAttribute('href','Product_page.html?name=Колумбия')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Колумбия";
+
+            a = document.querySelector('#all4')
+            a.setAttribute('href','Product_page.html?name=Коста Рика')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Коста Рика";
+
+            a = document.querySelector('#all5')
+            a.setAttribute('href','Product_page.html?name=Куба')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Куба";
+
+            a = document.querySelector('#all6')
+            a.setAttribute('href','Product_page.html?name=Эфиопия')
+            a.querySelector("#img").setAttribute('src','img/second_img.png')
+            a.querySelector('#p').innerHTML = "Эфиопия";
+
+
         }
-    )
+        else{
+            let a = document.querySelector('#all1')
+            a.setAttribute('href','Product_page.html?name=Кения Киамбу АВ')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Кения Киамбу АВ";
+
+            a = document.querySelector('#all2')
+            a.setAttribute('href','Product_page.html?name=Колумбия Киндио')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Колумбия Киндио";
+
+            a = document.querySelector('#all3')
+            a.setAttribute('href','Product_page.html?name=Коста Рика Таррасу')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Коста Рика Таррасу";
+
+            a = document.querySelector('#all4')
+            a.setAttribute('href','Product_page.html?name=Минас-Жерайс')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Минас-Жерайс";
+
+            a = document.querySelector('#all5')
+            a.setAttribute('href','Product_page.html?name=Эфиопия Анасора')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Эфиопия Анасора";
+
+            a = document.querySelector('#all6')
+            a.setAttribute('href','Product_page.html?name=Колумбия Киндио')
+            a.querySelector("#img").setAttribute('src','img/third_img.png')
+            a.querySelector('#p').innerHTML = "Колумбия Киндио";
+
+        }
+    })
 }
 
 let name = (new URL(document.URL)).searchParams.get('name');
