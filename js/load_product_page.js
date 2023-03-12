43051123daa59725c82ac02ab03a7f306fa9d2cc
lapis <klimantovartem@gmail.com>
2023-03-12 20:24:18 +0300

    ProductPage buy button logic

--- js/load_product_page.js
+++ js/load_product_page.js
@@ -187,6 +187,8 @@ function load(name) {
 let name = (new URL(document.URL)).searchParams.get('name');
 load(name);
 
+/*
 document.querySelector('.order-button').addEventListener('click', () => {
     alert(document.querySelector('.gram-number').value);
 })
+*/
---
+++ js/shopcard_add.js
@@ -0,0 +1,44 @@
+function identic(a,b){
+    if (a.Name === b.Name && a.Count === b.Count && a.Massa === b.Massa){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let uurl = (new URL(document.URL));
+
+if (uurl.href.indexOf('Product_page.html') > 0){
+    document.querySelector('.order-button').addEventListener('click', () => {
+        let newOrder = [{
+            Name : document.querySelector("#name_main").innerHTML,
+            Price : document.querySelector("#price_main").innerHTML,
+            Count : document.querySelector('#buttonCountNumber').innerHTML,
+            Massa : document.querySelector('.gram-number').value
+        }];
+
+        let order = localStorage.getItem('order');
+        if (order === null){
+            order = newOrder;
+            localStorage.setItem('order', JSON.stringify(order));
+        }else{
+            order = JSON.parse(order);
+            let flag=true;
+            for (key in order){
+                if (key === '0'){
+                    if (identic(order[key], newOrder[0])){
+                        flag = false;
+                    }
+                }else{
+                    if (identic(order[key][0], newOrder[0])){
+                        flag = false;
+                    }
+                }
+            }
+            if (flag){
+                order.push(newOrder);
+                localStorage.setItem('order', JSON.stringify(order));
+            }
+        }
+    })
+}
