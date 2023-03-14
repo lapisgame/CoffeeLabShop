7a064f3856866e3f075c3fc70450fbda9b0ef40a
lapis <klimantovartem@gmail.com>
2023-03-15 01:00:59 +0300

    add button logic catalog page

--- js/shopcard_add.js
+++ js/shopcard_add.js
@@ -1,45 +1,64 @@
-function identic(a,b){
-    if (a.Name === b.Name && a.Count === b.Count && a.Massa === b.Massa){
+function identic(a, b) {
+    if (a.Name === b.Name && a.Count === b.Count && a.Massa === b.Massa) {
         return true;
-    }else{
+    } else {
         return false;
+    }
+}
+
+function addToLocalStorage(newOrder) {
+    let order = localStorage.getItem('order');
+    if (order === null) {
+        order = newOrder;
+        localStorage.setItem('order', JSON.stringify(order));
+    } else {
+        order = JSON.parse(order);
+        let flag = true;
+        for (key in order) {
+            if (key === '0') {
+                if (identic(order[key], newOrder[0])) {
+                    flag = false;
+                }
+            } else {
+                if (identic(order[key][0], newOrder[0])) {
+                    flag = false;
+                }
+            }
+        }
+        if (flag) {
+            order.push(newOrder);
+            localStorage.setItem('order', JSON.stringify(order));
+        }
     }
 }
 
 let uurl = (new URL(document.URL));
 
-if (uurl.href.indexOf('Product_page.html') > 0){
+if (uurl.href.indexOf('Product_page.html') > 0) {
     document.querySelector('#order-button').addEventListener('click', () => {
         let newOrder = [{
-            Name : document.querySelector("#name_main").innerHTML,
-            Price : document.querySelector("#price_main").innerHTML,
-            Count : document.querySelector('#buttonCountNumber').innerHTML,
-            Massa : document.querySelector('.gram-number').value,
-            Type : (new URL(url)).searchParams.get('type')
+            Name: document.querySelector("#name_main").innerHTML,
+            Price: document.querySelector("#price_main").innerHTML,
+            Count: document.querySelector('#buttonCountNumber').innerHTML,
+            Massa: document.querySelector('.gram-number').value,
+            Type: uurl.searchParams.get('type')
         }];
+        addToLocalStorage(newOrder);
+    })
+}
 
-        let order = localStorage.getItem('order');
-        if (order === null){
-            order = newOrder;
-            localStorage.setItem('order', JSON.stringify(order));
-        }else{
-            order = JSON.parse(order);
-            let flag=true;
-            for (key in order){
-                if (key === '0'){
-                    if (identic(order[key], newOrder[0])){
-                        flag = false;
-                    }
-                }else{
-                    if (identic(order[key][0], newOrder[0])){
-                        flag = false;
-                    }
-                }
-            }
-            if (flag){
-                order.push(newOrder);
-                localStorage.setItem('order', JSON.stringify(order));
-            }
-        }
+if (uurl.href.indexOf('catalog.html') > 0){
+    let buttons = document.querySelectorAll('#order-button');
+    buttons.forEach((el) => {
+        el.addEventListener('click', () => {
+            let newOrder = [{
+                Name: el.parentElement.querySelector('#name').innerHTML,
+                Price: el.parentElement.querySelector('#price').innerHTML,
+                Count: "1",
+                Massa: '250гр',
+                Type: uurl.searchParams.get('type')
+            }];
+            addToLocalStorage(newOrder);
+        })
     })
 }
